011971a4f0b879a54b5b6b9f2386cd00f72ef248
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-12-22 03:35:33 +0100

    Update descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -30,16 +30,10 @@ const dataRE = await fetch(`https://www.vanitas-api.online/download/ytmp3?url=${
 const dataRET = await dataRE.json();
 let n = dataRET.response.title || 'error'
 await conn.sendMessage(m.chat, { audio: { url: dataRET.response.link }, fileName: `${n}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })  
-} catch {
-try {
-let v = youtubeLink;
-const dataRE = await fetch(`https://www.vanitas-api.online/download/ytmp3?url=${v}`);
-const dataRET = await dataRE.json();
-await conn.sendMessage(m.chat, dataRET.response.link, 'default.mp3', null, m, false, { mimetype: 'audio/mpeg' });
 } catch (e) {
 await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)}
-}}
+}
 handler.command = /^audio|fgmp3|dlmp3|getaud|yt(a|mp3)$/i
 export default handler
